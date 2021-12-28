f069bc34b8dff62456771409e99a7d267324cbb3
bartoz1 <nil112101@gmail.com>
2021-12-28 22:30:55 +0100

    Wersja wyslana na enauczanie

--- KASK_DEL_UPD.sql
+++ KASK_DEL_UPD.sql
@@ -1,17 +1,20 @@
---SELECT * FROM Kategorie
+--KASKADOWE UAKTUALNIENIE KATEGORII PRODUKTU PO ZMIANIE NAZWY KATEGORII
 SELECT  * FROM Produkty
 UPDATE Kategorie SET nazwa='mechanika' where nazwa='motoryzacja'
 SELECT  * FROM Produkty
 
-select zamowienia.id, nazwa, ilosc, data_zlozenia, Reklamacje.opis from Zamowienia, Produkt_zamowienie, Produkty, Reklamacje
-	where Produkt_zamowienie.produkt_id = Produkty.id 
-	and Produkt_zamowienie.zamowienie_id = Zamowienia.ID 
-	and Zamowienia.ID in (SELECT zamowienie_id FROM Reklamacje)
-	and Zamowienia.ID = Reklamacje.zamowienie_id
-	ORDER BY Zamowienia.ID
 
+
+--KASKADOWE USUWANIE REKLAMACJI PO USUNIECIU ZAMOWIENIA DO KTOREGO ODNOSI SIE REKLAMACJA
 SELECT * FROM Zamowienia
 SELECT * FROM Reklamacje
 DELETE FROM Zamowienia where ID=4
 SELECT * FROM Reklamacje
 SELECT * FROM Zamowienia
+
+
+
+-- KASKADOWE USUWANIE Z PRODUKT_ZAMOWIENIE PO USUNIECIU ZAMOWIENIA
+SELECT * FROM Produkt_zamowienie where zamowienie_id <=5
+DELETE FROM Zamowienia where ID=4
+SELECT * FROM Produkt_zamowienie where zamowienie_id <=5
